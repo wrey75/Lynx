c90560b6afa391719a554b6fd6fb6879dbeb1733
William Rey <wrey@free.fr>
2013-11-21 17:30:37 +0100

    Changed POM.XML to produce a complete JAR.

---
+++ src/main/java/com/oxande/lynx/LynX.java
@@ -0,0 +1,11 @@
+package com.oxande.lynx;
+
+import com.jcraft.weirdx.WeirdX;
+
+public class LynX {
+
+	public static void main(String args[]) {
+		// Redirect to WeirdX
+		WeirdX.main(args);
+	}
+}
---
+++ src/main/java/com/oxande/lynx/Utils.java
@@ -0,0 +1,10 @@
+package com.oxande.lynx;
+
+/**
+ * A generic class for utilities needed for the project.
+ * 
+ *
+ */
+public final class Utils {
+	
+}
